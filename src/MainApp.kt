2fc8e61218f16b55cc9af2267996e185b4d17f46
iyan <supian22@gmail.com>
2025-09-13 10:42:41 +0700

    Praktikum 1

---
+++ src/MainApp.kt
@@ -0,0 +1,12 @@
+fun main (args: Array<String>) {
+    var intLiteral = 5
+    var doubleLiteral = 0.2
+    var StringLiteral = "Hello"
+    var charLiteral = "1"
+    var boolLiteral = true
+    println("intLiteral = " + intLiteral)
+    println("doubleLiteral = " + doubleLiteral)
+    println("StringLiteral = " + StringLiteral)
+    println("charLiteral = " + charLiteral)
+    println("boolLiteral = " + boolLiteral)
+}
